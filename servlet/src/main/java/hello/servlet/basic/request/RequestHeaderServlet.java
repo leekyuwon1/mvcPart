ca8f18015b2b559d7316355fab121aa6036a1818
leekyuwon <sjin9574@naver.com>
2024-03-08 21:34:27 +0900

    2240308

--- servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -17,6 +17,7 @@ public class RequestHeaderServlet extends HttpServlet {
         printStartLine(request);
         printHeaders(request);
         printHeaderUtils(request);
+        printEtc(request);
     }
 
     private static void printStartLine(HttpServletRequest request) {
@@ -47,6 +48,7 @@ public class RequestHeaderServlet extends HttpServlet {
 */
         request.getHeaderNames().asIterator()
                         .forEachRemaining(headerName -> System.out.println(headerName + ": " + headerName));
+//        request.getHeader("host");
         System.out.println("--- Headers - end ---");
         System.out.println();
     }
@@ -81,6 +83,24 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getCharacterEncoding() = " +
                 request.getCharacterEncoding());
         System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+    }
+
+    private void printEtc(HttpServletRequest request) {
+        System.out.println("--- 기타 조회 start ---");
+        System.out.println("[Remote 정보]");
+        System.out.println("request.getRemoteHost() = " +
+                request.getRemoteHost()); //
+        System.out.println("request.getRemoteAddr() = " +
+                request.getRemoteAddr()); //
+        System.out.println("request.getRemotePort() = " +
+                request.getRemotePort()); //
+        System.out.println();
+        System.out.println("[Local 정보]");
+        System.out.println("request.getLocalName() = " + request.getLocalName()); //
+        System.out.println("request.getLocalAddr() = " + request.getLocalAddr()); //
+        System.out.println("request.getLocalPort() = " + request.getLocalPort()); //
+        System.out.println("--- 기타 조회 end ---");
         System.out.println();
     }
 }
