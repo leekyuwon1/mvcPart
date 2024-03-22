557df1e4e46894e0041e799792fc4a92edeea7cc
leekyuwon <sjin9574@naver.com>
2024-03-22 23:39:56 +0900

    2240322

--- servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -1,5 +1,6 @@
-package hello.servlet.web;
+package hello.servlet.web.servlet;
 
+import hello.servlet.domain.member.MemberRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,6 +12,8 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
 public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
