9085eb3e9209f4c76622953a8bf2c9b863796f5d
leekyuwon <sjin9574@naver.com>
2024-03-30 22:30:21 +0900

    2240330

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,9 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>();
+}
