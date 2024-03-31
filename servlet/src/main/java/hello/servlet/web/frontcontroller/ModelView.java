a3a4d7045e37790cacd9e78f8388aae1909cc7ab
leekyuwon <sjin9574@naver.com>
2024-03-31 22:48:47 +0900

    2240331

--- servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -6,4 +6,26 @@ import java.util.Map;
 public class ModelView {
     private String viewName;
     private Map<String, Object> model = new HashMap<>();
+
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public ModelView setViewName(String viewName) {
+        this.viewName = viewName;
+        return this;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public ModelView setModel(Map<String, Object> model) {
+        this.model = model;
+        return this;
+    }
 }
